8732471680eb928924c17095c21c6c2182aa4708
ekelman <ekelman@hotmail.com>
2015-01-29 20:50:21 -0600

    Fixed model

--- Food4Life/Food4Life/Models/superhealthyeats.Designer.cs
+++ Food4Life/Food4Life/Models/superhealthyeats.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\SBG225\Desktop\Food4Life\Food4Life\Food4Life\Models\superhealthyeats.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alla\Desktop\Food4Life\Food4Life\Food4Life\Models\superhealthyeats.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
